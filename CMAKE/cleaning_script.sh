7ac17cfd1f0289a128f3f723277b344fc5fd6772
Gajanan Choudhary <gajanan@utexas.edu>
2020-06-06 18:16:14 -0500

    Fix cleaning script

--- CMAKE/cleaning_script.sh
+++ CMAKE/cleaning_script.sh
@@ -1,1 +1,1 @@
-rm Makefile cmake_install.cmake CMakeCache.txt bin/laplace && rm -r CMakeFiles/ initio/ lib/ main/ solver/ structs/ messaging/
+rm Makefile cmake_install.cmake CMakeCache.txt bin/laplace && rm -r CMakeFiles/ initio/ lib/ main/ solver/ structs/ messaging/ tests/
